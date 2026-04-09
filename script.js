c194a5442176742c6cfafaadbedfb45dd5e89723
varre-x <nelsonjoshua.arrak@techno.ee>
2026-04-09 13:04:46 +0300

    trees

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const text = document.getElementsByClassName("text")[0];
 const container = document.getElementById("container");
+const lobby = document.getElementById("lobby");
+const nameField = document.getElementById("nameField");
+const joinBtn = document.getElementById("joinBtn");
 
-async function loadHexMap() {
+async function fetchData() {
     const response = await fetch('https://tinkr.tech/sdb/antiyoy/antiyoyDB', {
         method: 'GET'
     });
-    data = await response.json();
-    console.log(data);
+    return await response.json();
+}
+
+async function loadHexMap() {
+    const data = await fetchData();
+    container.innerHTML = "";
+console.log(data);
     const tiles = data.map
 
     for (const tile of tiles) {
@@ -18,9 +25,33 @@ async function loadHexMap() {
             hex.style.top = `${tile.y}px`;
 
             container.appendChild(hex);
-        } 
+
+            if (tile.building === "tree") {
+                const tree = document.createElement("img");
+                tree.src = `https://tinkr.tech//sdb_apps/antiyoy/images/tree.svg`;
+                tree.classList.add("tree");
+                tree.style.left = `${tile.x}px`;
+                tree.style.top = `${tile.y}px`;
+                container.appendChild(tree);    
+            } 
+        }
+        
     }
     return;
 };
 
+async function startGame() {
+    const data = await fetchData();
+}   
+
+startGame();
+//setInterval(loadHexMap, 2000);
+
 loadHexMap();
+
+joinBtn.addEventListener("click", () => {
+    const playerName = nameField.value.trim();
+    if (playerName !== "") {
+        lobby.classList.toggle("invisible");
+    }
+});
